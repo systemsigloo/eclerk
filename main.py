5e39629d0f1c16f75ccf19e5b6b53622cb68d57b
colosimokid <jesusgomezuribe@gmail.com>
2026-04-07 16:36:27 -0400

    cambios login desde oficina

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from routers import user
+from routers import user, login
 
 
-
-
+#pip3.14 install "pwdlib[argon2]"
+#pip3.14 install pyjwt   
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(login.router)
 
 @app.get("/")
 async def root():
